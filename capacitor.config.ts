67a920e5577e72332eb034499d9c778878983781
claudiolz <claudio.lizama2015@alu.uct.cl>
2022-11-03 19:19:29 -0300

    Cambio nombre App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'GoogleMapsApi',
+  appName: 'App FR',
   webDir: 'www',
   bundledWebRuntime: false
 };
