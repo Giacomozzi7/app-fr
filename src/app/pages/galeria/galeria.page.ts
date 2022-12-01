6b8b03f190cdab50ed6d56ff6a29603a3382ef09
Francisco Giacomozzi <44447339+Giacomozzi7@users.noreply.github.com>
2022-12-01 18:29:21 -0300

    implementacion vista de videos

--- src/app/pages/galeria/galeria.page.ts
+++ src/app/pages/galeria/galeria.page.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Galeria } from 'src/app/interfaces/interfaces';
 import { ProveedorService } from 'src/app/services/proveedor.service';
-
+import { IonSlides } from '@ionic/angular';
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.page.html',
@@ -10,83 +10,106 @@ import { ProveedorService } from 'src/app/services/proveedor.service';
 })
 export class GaleriaPage implements OnInit {
   profileId: string;
-  refMemoria:string;
-  galeria : Galeria[];
-  refAgregarImagen: string;
-  myImgStr: string = 'Mis Imágenes';
-  myImg : boolean = false;
-  userId = "632a072930305800b2d85221"
-  refEditarImagen: string;
-
+  refMemoria: string;
+  galeria: Galeria[];
+  refAgregar: string;
+  refEditar: string;
+  
+  myImg: boolean = false;
+  userId: string = '632a072930305800b2d85221';
+  
   misImgs: boolean = false;
+  flagGaleria:  boolean = true;
+
+  //Segment changes
+  strMiGaleria: string = 'Mis Imágenes';
+  strAgregar: string = 'Agregar Imágen'
+  strParagraph: string = 'todas las imágenes subidas'
+  strIcon: string = 'images'
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    public proveedor: ProveedorService,
-  ) { }
+    public proveedor: ProveedorService
+  ) {}
 
   ngOnInit() {
     this.profileId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.refMemoria = 'memoria/'+ this.profileId;
-    this.refAgregarImagen = 'agregar-imagen/' + this.profileId + '/agregar/""'
-    this.refEditarImagen = 'agregar-imagen/' + this.profileId + '/editar/'
-    
-    this.obtGaleria()
-    
+    this.refMemoria = 'memoria/' + this.profileId;
+    this.refAgregar = 'agregar-imagen/' + this.profileId + '/agregar/""';
+    this.refEditar = 'agregar-imagen/' + this.profileId + '/editar/';
+
+    this.obtGaleria();
   }
 
-  fillLikes(){
-    this.galeria = this.galeria.map((element: Galeria) =>{
-      if (element.likes.includes(this.userId)){
+  fillLikes() {
+    this.galeria = this.galeria.map((element: Galeria) => {
+      if (element.likes.includes(this.userId)) {
         return {
           ...element,
-          like_name: 'heart'
-        }
-      } 
-      else {
+          like_name: 'heart',
+        };
+      } else {
         return {
           ...element,
-          like_name: 'heart-outline'
-        }
+          like_name: 'heart-outline',
+        };
       }
-    })
+    });
   }
 
-  obtGaleria(){
-    this.proveedor.obtenerGaleria(this.profileId)
-      .subscribe((data) => {
-        this.galeria = data[0].galeria;
-        this.buscarUsuarios()
-        this.fillLikes()
-      });
-
+  obtGaleria() {
+    this.proveedor.obtenerGaleria(this.profileId).subscribe((data) => {
+      this.galeria = data[0].galeria;
+      this.buscarUsuarios();
+      this.fillLikes();
+    });
   }
 
-
-  toggleImagenes(){
-    if (this.myImg === false){
-      this.galeria = this.galeria.filter(obj => obj.usuario_id === this.userId)
+  toggleImagenes() {
+    if (this.myImg === false) {
+      this.galeria = this.galeria.filter(
+        (obj) => obj.usuario_id === this.userId
+      );
       this.myImg = true;
-      this.myImgStr = 'Todos';
+      this.strMiGaleria = 'Todos';
       this.misImgs = true;
-    } else{
-      this.obtGaleria()
+    } else {
+      this.obtGaleria();
       this.myImg = false;
-      this.myImgStr = 'Mis Imágenes'
+      this.flagGaleria
+        ? this.strMiGaleria = 'Mis Imágenes'
+        : this.strMiGaleria = 'Mis Videos'
+
       this.misImgs = false;
     }
   }
 
-  buscarUsuarios(){
+  buscarUsuarios() {
     for (let i = 0; i < this.galeria.length; i++) {
-      let userId = this.galeria[i]['usuario_id']
-      this.proveedor.obtenerUsuario(userId)
-        .subscribe((usuario) => {
-          let strNombre = usuario[0]['nombre'] + " "+ usuario[0]['apellido']
-          this.galeria[i]['usuario_name'] = strNombre
-        })
-   }
+      let userId = this.galeria[i]['usuario_id'];
+      this.proveedor.obtenerUsuario(userId).subscribe((usuario) => {
+        let strNombre = usuario[0]['nombre'] + ' ' + usuario[0]['apellido'];
+        this.galeria[i]['usuario_name'] = strNombre;
+      });
+    }
+  }
+
+  async segmentChanged() {
+    this.flagGaleria = await !this.flagGaleria
+    if (this.flagGaleria){
+      this.strMiGaleria = 'Mis Imágenes'
+      this.strAgregar = 'Agregar Imágen'
+      this.strParagraph = 'todas las imágenes subidas'
+      this.strIcon = 'images'
+    } else{
+      this.strMiGaleria = 'Mis Videos'
+      this.strAgregar = 'Agregar Video'
+      this.strParagraph = 'todos los videos subidos'
+      this.strIcon = 'videocam'
+    }
 
   }
+
 
 }
